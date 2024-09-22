dd77d40f298a55dc1d07689800f85ed63f7b1ce8
Quizzers' Club NIT Bhopal <95757569+quizzersclubmanit@users.noreply.github.com>
2024-09-22 13:22:11 +0530

    UI tweaks

--- src/components/SectionHead.jsx
+++ src/components/SectionHead.jsx
@@ -21,12 +21,12 @@ const SectionHead = ({
         />
       )}
       <h2
-        className={`text-[5vmax] poppins-bold flex flex-col md:flex-row items-center justify-start  gap-2 ${outline && "text-outline"} ${className}`}
+        className={`text-[5vmax] poppins-bold flex flex-col md:flex-row items-center justify-center  gap-2 ${outline && "text-outline"} ${className}`}
       >
         <h2>{label}</h2>
 
         {label=="Register" && (
-          <p className="text-xs  text-red-600 self-start  md:mt-10 md:self-auto"> (Completely Free) </p>
+          <p className="text-[22px] overflow-y-hidden text-blue-800  md:mt-10 md:self-auto"> (Completely Free) </p>
         )}
 
       </h2>
