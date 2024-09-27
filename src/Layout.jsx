90565321c69a7003182ef01d5309326021dd2cd2
pecee06 <priyanshu.choudhury2406@outlook.com>
2024-09-27 14:05:13 +0530

    add analytics

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -2,8 +2,10 @@ import { Outlet } from "react-router-dom"
 import { Toaster } from "react-hot-toast"
 import { useState, useEffect } from "react"
 import { PopupModal } from "./components/components.js"
+import { Analytics } from "@vercel/analytics/react"
+
 const Layout = () => {
-  const [showModal, setShowModal] = useState(false);
+  const [showModal, setShowModal] = useState(false)
 
   document.addEventListener("copy", (e) => {
     e.preventDefault()
@@ -15,21 +17,18 @@ const Layout = () => {
   // Trigger the modal after 1 seconds
   useEffect(() => {
     const timer = setTimeout(() => {
-      setShowModal(true);
-    }, 1000); // 1 seconds
+      setShowModal(true)
+    }, 1000) // 1 seconds
 
     // Cleanup timer on unmount
-    return () => clearTimeout(timer);
-  }, []);
+    return () => clearTimeout(timer)
+  }, [])
   return (
     <>
       <Outlet />
-      {showModal && (
-        <PopupModal setShowModal={setShowModal}>
-        
-        </PopupModal>
-      )}
+      {showModal && <PopupModal setShowModal={setShowModal}></PopupModal>}
       <Toaster position="top-center" toastOptions={{ duration: 4000 }} />
+      <Analytics />
     </>
   )
 }
