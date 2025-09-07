5384ca524e0adf9e090693bc89b1bf649f65d139
Quizzers' Club NIT Bhopal <95757569+quizzersclubmanit@users.noreply.github.com>
2025-09-07 20:26:16 +0530

    Update store.service.js

--- src/api/store.service.js
+++ src/api/store.service.js
@@ -1,46 +1,22 @@
-import { Client, Storage, ID } from "appwrite"
-import env from "../../constants"
+// Storage service - Appwrite backend calls removed
 
 class Store {
-  client = new Client()
-  storage
-
-  constructor() {
-    this.client.setEndpoint(env.apiEndpoint).setProject(env.projectId)
-
-    this.storage = new Storage(this.client)
-  }
-
   async uploadFile({ file }) {
-    if (!file || typeof file == "string") return null
-    try {
-      const res = await this.storage.createFile(
-        env.questionBucketId,
-        ID.unique(),
-        file
-      )
-      return res
-    } catch (error) {
-      throw error
-    }
+    // TODO: Implement alternative file storage method
+    console.warn('Store service: uploadFile method disabled - Appwrite backend removed')
+    return null
   }
 
   async deleteFile({ fileId }) {
-    try {
-      const res = await this.storage.deleteFile(env.questionBucketId, fileId)
-      return res
-    } catch (error) {
-      throw error
-    }
+    // TODO: Implement alternative file storage method
+    console.warn('Store service: deleteFile method disabled - Appwrite backend removed')
+    return null
   }
 
-  fetchFilePreview({ fileId, bucketId = env.questionBucketId }) {
-    try {
-      const res = this.storage.getFilePreview(bucketId, fileId)
-      return res.toString()
-    } catch (error) {
-      throw error
-    }
+  fetchFilePreview({ fileId, bucketId }) {
+    // TODO: Implement alternative file preview method
+    console.warn('Store service: fetchFilePreview method disabled - Appwrite backend removed')
+    return null
   }
 }
 
