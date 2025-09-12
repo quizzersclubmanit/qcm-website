3ef4af7e17095118516a3ffdcb0b1076641fa834
Aksh <akshshah0907@gmail.com>
2025-09-12 15:02:25 +0530

    fixing mpstdc logo

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -1,15 +1,15 @@
-import qcmLogo from "./qcm-logo.png"
-import bulb from "./bulb.png"
-import authIllustration from "./auth-illustration.png"
-import gradientLogo from "./gradient-qcm-logo.png"
-import floatingMarks from "./floating-mark.png"
-import aboutIllus from "./illus.jpg"
-import liveGif from "./live.gif"
-import manit from "./manit.jpg"
-import team from "./team.jpg"
-import poster from "./compressedPoster.jpg"
-import myglamm from "./myglamm.jpg"
-import slots from "./slots.png"
+import qcmLogo from "./qcm-logo.png";
+import bulb from "./bulb.png";
+import authIllustration from "./auth-illustration.png";
+import gradientLogo from "./gradient-qcm-logo.png";
+import floatingMarks from "./floating-mark.png";
+import aboutIllus from "./illus.jpg";
+import liveGif from "./live.gif";
+import manit from "./manit.jpg";
+import team from "./team.jpg";
+import poster from "./compressedPoster.jpg";
+import myglamm from "./myglamm.jpg";
+import slots from "./slots.png";
 
 export {
   qcmLogo,
@@ -23,5 +23,5 @@ export {
   team,
   poster,
   myglamm,
-  slots
-}
+  slots,
+};
