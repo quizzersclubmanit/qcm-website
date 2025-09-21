815accb0347b7572277c034afb11eeae8b2a5664
Quizzers' Club NIT Bhopal <95757569+quizzersclubmanit@users.noreply.github.com>
2025-09-21 20:49:08 +0530

    Update Quiz.jsx

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -461,9 +461,6 @@ const Quiz = ({ quiz = {}, setShowModal = () => {} }) => {
             <option value={2}>2</option>
             <option value={3}>3</option>
             <option value={4}>4</option>
-            <option value={5}>5</option>
-            <option value={6}>6</option>
-            <option value={7}>7</option>
           </select>
         </div>
         <div className="flex gap-5 items-center bg-pink-300 p-2 rounded-lg">
